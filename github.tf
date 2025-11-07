536ecd070304370a5ffbcf48b633ba19cc8ad8f1
Pavithra2294 <pav.ganesan@gmail.com>
2025-11-07 00:35:00 +0000

    Update github.tf

--- github.tf
+++ github.tf
@@ -2,8 +2,8 @@ provider "github" {
   owner = "Pavithra2294"
 }
 
-resource "github_repository" "demo_repo" {
-  name        = "my-terraform-created-repo-demo11"
+resource "github_repository" "demo_repo11" {
+  name        = "my-terraform-created-repo-demo1111"
   description = "This repository was created using Terraform!"
   visibility  = "public"    # or "private"
   auto_init   = true        # creates an initial commit with README
