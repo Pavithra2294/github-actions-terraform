217a45f8eaaeca4d5c031caa9345bb54d0179275
Pavithra2294 <pav.ganesan@gmail.com>
2025-11-07 17:14:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,33 +2,28 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "pavithra-terraform-demo-bucket2"
-}
-
-
 # S3 Bucket for Source Data
-resource "aws_s3_bucket" "source-data-bucket" {
-  bucket        = "source-data-bucket"
+resource "aws_s3_bucket" "source-data-bucket-pre-finance" {
+  bucket        = "source-data-bucket-pre-finance"
   force_destroy = true
 }
 
 
 # S3 Bucket for Traget 
-resource "aws_s3_bucket" "target-data-bucket" {
-  bucket        = "target-data-bucket"
+resource "aws_s3_bucket" "target-data-bucket-pre-finance" {
+  bucket        = "target-data-bucket-pre-finance"
   force_destroy = true
 }
 
 
-resource "aws_s3_bucket" "code_bucket_paithra11" {
-  bucket = "code-bucket-paithra1"   # real AWS bucket name (hyphens OK here)
+resource "aws_s3_bucket" "code-bucket-pre-finance" {
+  bucket = "code-bucket-pre-finance" 
   acl    = "private"
 }
 
 
-resource "aws_s3_object" "script_file11" {
-  bucket = aws_s3_bucket.code_bucket_paithra11.id
+resource "aws_s3_object" "script_file-pre-finance" {
+  bucket = aws_s3_bucket.code-bucket-pre-finance.id
   key    = "script.py"           
   source = "script.py"  
 }
